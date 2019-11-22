776e2a0314695b72d093eabaab9acecbf1921ad8
dinh2350 <dinh2350@gmail.com>
2019-11-22 20:30:22 +0700

    them

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,5 +1,9 @@
 var nguoiDungService = new NguoiDungService();
 
+function thehao() {
+  console.log("them thành công 2");
+}
+
 getListUser();
 
 getEle("btnThemNguoiDung").addEventListener("click", function() {
